83f68300e4061091ee3ac080fa9c0f5a8cd18ba5
Plalangan <P.lalangan@hotmail.com>
2019-11-23 12:57:12 -0800

    Added express routing
    added express routing and cleaned up folder

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express'),
+app = express(),
+morgan = require('morgan');
+
+let topmovies = [{
+  title: 'Avengers'
+},
+{
+  title: 'Avengers: Age of Ultron'
+},
+{
+  title: 'Avengers: Infinity War'
+},
+{
+  title: 'Avengers: Endgame'
+},
+{
+  title: 'The Departed'
+},
+{
+  title: 'Batman Begins'
+},
+{
+  title: 'The Dark Knight'
+},
+{
+  title: 'The Dark Knight Rises'
+},
+{
+  title: 'Thor: Ragnarok'
+},
+{
+  title: 'Captain America: Civil War'
+}];
+
+
+app.get('/movies', function(req, res) {
+  res.json(topmovies)
+});
+
+app.get('/', function(req, res){
+  res.send('Hello welcome to my page!')
+});
+
+express.static('public');
+
+app.use(morgan('common'));
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('There is an error!');
+});
