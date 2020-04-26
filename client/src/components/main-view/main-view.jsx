472a15aa32858488cf4ee46eeac1bd2b2ca14eec
Plalangan <P.lalangan@hotmail.com>
2020-04-26 14:20:10 -0700

    created components
    created react components

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
-    constructor(){
-        //Call the superclass constructor so react can initialize it
-        super();
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
-        //Initialize the state to an empty object so we can destructure it later
-        this.state = {};
+export class MainView extends React.Component {
+
+    constructor() {
+      super();
+  
+      this.state = {
+        movies: null,
+        selectedMovie: null
+      };
     }
-
-        //This overrides the render() method of the superclass
-        //No need to call super() though, as it does nothing by default
-        render(){
-            return(
-                <div className="main-view"></div>
-            )
+  
+    componentDidMount() {
+      /* ... */
+    }
+  
+    onMovieClick(movie) {
+      this.setState({
+        selectedMovie: movie
+      });
+    }
+  
+  
+    render() {
+      const { movies, selectedMovie } = this.state;
+  
+      // Before the movies have been loaded
+      if (!movies) return <div className="main-view"/>;
+  
+      return (
+       <div className="main-view">
+        {selectedMovie
+           ? <MovieView movie={selectedMovie}/>
+           : movies.map(movie => (
+             <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+           ))
         }
-}
+       </div>
+      );
+    }
+  }
