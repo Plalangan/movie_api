dc9223285e38d2fe5d10b7645c9f5c45a22d5540
Plalangan <P.lalangan@hotmail.com>
2020-04-28 13:14:02 -0700

    Added loginview and registrationview
    added loginview and registrationview components

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
+import  {MovieCard}  from '../movie-card/movie-card';
+import  {MovieView}  from '../movie-view/movie-view';
+import  {LoginView}  from '../login-view/login-view';
 
 export class MainView extends React.Component {
 
@@ -11,23 +12,41 @@ export class MainView extends React.Component {
   
       this.state = {
         movies: null,
-        selectedMovie: null
+        selectedMovie: null,
+        user: null
       };
     }
   
     componentDidMount() {
-      /* ... */
-    }
+        axios.get('https://myflixdb-pl.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   
     onMovieClick(movie) {
       this.setState({
         selectedMovie: movie
       });
     }
+    
+    onLoggedIn(user){
+      this.setState({
+        user
+      });
+    }
   
   
     render() {
-      const { movies, selectedMovie } = this.state;
+      const { movies, selectedMovie, user } = this.state;
+
+      if (!user) return <LoginView onLoggedIn={user => this.oonLoggedIn(user)}/>;
   
       // Before the movies have been loaded
       if (!movies) return <div className="main-view"/>;
