23943ac279ac049603db7eebb8bc8eb7fd72df1b
Plalangan <P.lalangan@hotmail.com>
2020-04-30 01:31:25 -0700

    cors fix

--- auth.js
+++ auth.js
@@ -13,8 +13,7 @@ function generateJWTToken(user){
 
 // POST login
 module.exports = (router) => {
-  router.post('/login',
-  {headers: {"Access-Control-Allow-Origin": "*"}}, (req, res) => {
+  router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false},(error, user, info) => {
     if (error || !user) {
       return res.status(400).json({
