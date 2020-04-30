a0996084a59051b814c75269e887927855a8536e
Plalangan <P.lalangan@hotmail.com>
2020-04-30 15:50:16 -0700

    route fix

--- auth.js
+++ auth.js
@@ -13,7 +13,7 @@ function generateJWTToken(user){
 
 // POST login
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
+  router.get('/login', (req, res) => {
     passport.authenticate('local', { session: false},(error, user, info) => {
     if (error || !user) {
       return res.status(400).json({
