685621c6cbd493caedb49e5dd0d21d5af1503dbe
Plalangan <P.lalangan@hotmail.com>
2020-05-28 20:03:27 -0700

    fix

--- passport.js
+++ passport.js
@@ -1,18 +1,18 @@
 const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy;
+  LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
 
-var Users = models.User;
+var Users = Models.User;
 var JWTStrategy = passportJWT.Strategy;
 var ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
  usernameField: 'Username',
  passwordField: 'Password'
-}, (Username, Password, callback) => {
+}, (username, password, callback) => {
  console.log(username + '  ' + password);
- Users.findOne({ Username: Username }, (error, user) => {
+ Users.findOne({ Username: username }, (error, user) => {
    if (error) {
      console.log(error);
      return callback(error);
@@ -21,7 +21,7 @@ passport.use(new LocalStrategy({
      console.log('incorrect username');
      return callback(null, false, {message: 'Incorrect username.'});
    }
-   if(!user.validatePassword(Password)){
+   if(!user.validatePassword(password)){
      console.log('incorrect password');
      return callback(null, false, {message: 'Incorrect password'});
    }
